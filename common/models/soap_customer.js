4a7eb98f3511d9ef192f4e6615d764eef22df592
faisal <faizalyudhistira@gmail.com>
2018-03-04 02:33:37 -0800

    kerjainlokal

--- common/models/soap_customer.js
+++ common/models/soap_customer.js
@@ -207,4 +207,160 @@ module.exports = function (Soapcustomer) {
       }
     });
 
+    Soapcustomer.getListBTNConsumerPKToday = function (datetoday, cb) {
+      const a = moment(datetoday, 'YYYYMMDD');
+      Soapcustomer.GetListBTNConsumer({
+        Data: 'PK',
+        Tanggal: a.format('YYYYMMDD'), //yyymmdd
+        uid: "$up3rPAN"
+      }, function (err, response) {
+        var result = JSON.parse(response.GetListBTNConsumerResult);
+        var Mstcustomerpk = app.models.MST_CUSTOMER_PK;
+        for (var data in result.data) {
+          Mstcustomerpk.create({
+            "CUSTOMER_ID": result.data[data].NO_APLIKASI,
+            "KODE_CABANG": result.data[data].KODE_CABANG,
+            "CABANG": result.data[data].CABANG,
+            "KANWIL": result.data[data].KANWIL,
+            "NO_APLIKASI": result.data[data].NO_APLIKASI,
+            "NAMA_DEBITUR": result.data[data].NAMA_DEBITUR,
+            "GENDER": result.data[data].GENDER,
+            "ALAMAT_DEBITUR": result.data[data].ALAMAT_DEBITUR,
+            "ALAMAT_AGUNAN": result.data[data].ALAMAT_AGUNAN,
+            "TANGGAL_LAHIR": result.data[data].TANGGAL_LAHIR,
+            "USIA": parseInt(result.data[data].USIA),
+            "PK_DATE": result.data[data].PK_DATE,
+            "JANGKA_WAKTU": result.data[data].JANGKA_WAKTU,
+            "JENIS_KREDIT": result.data[data].JENIS_KREDIT,
+            "PLAFOND_KREDIT": parseInt(result.data[data].PLAFOND_KREDIT),
+            "HARGA_BANGUNAN": parseInt(result.data[data].HARGA_BANGUNAN),
+            "PEKERJAAN": result.data[data].PEKERJAAN,
+            "ASURANSI_JIWA": parseInt(result.data[data].ASURANSI_JIWA),
+            "ASURANSI_FIRE": parseInt(result.data[data].ASURANSI_FIRE),
+            "ASURANSI_KREDIT": parseInt(result.data[data].ASURANSI_KREDIT),
+            "DATE_TIME_CREATE": "2018-02-14T11:40:48.637Z"
+          }, function (err, inst) {
+  
+          });
+        };
+       
+       var data={
+         message: result.message,
+         error:err||null
+       }
+        cb(err, data);
+      });
+    };
+  
+    Soapcustomer.remoteMethod(
+      'getListBTNConsumerPKToday', {
+        accepts: {
+          arg: 'datetoday',
+          type: 'string'
+        },
+        returns: {
+          arg: 'data',
+          type: 'object',
+          root: true
+        },
+        http: {
+          verb: 'post',
+          path: '/getlistconsumerPKtoday'
+        }
+      });
+
+    // Soapcustomer.getListBTNConsumerToday = function (datetoday, datatype, cb) {
+    //   const a = moment(datetoday, 'YYYYMMDD');
+    //   Soapcustomer.GetListBTNConsumer({
+    //     Data: datatype,
+    //     Tanggal: a.format('YYYYMMDD'), //yyymmdd
+    //     uid: "$up3rPAN"
+    //   }, function (err, response) {
+    //     var result = JSON.parse(response.GetListBTNConsumerResult);
+    //     var Mstcustomer = app.models.MST_CUSTOMER;
+    //     var Mstcustomerpk = app.models.MST_CUSTOMER_PK;
+    //     for (var data in result.data) {
+    //       Mstcustomerpk.create({
+    //         "CUSTOMER_ID": result.data[data].NO_APLIKASI,
+    //         "KODE_CABANG": result.data[data].KODE_CABANG,
+    //         "CABANG": result.data[data].CABANG,
+    //         "KANWIL": result.data[data].KANWIL,
+    //         "NO_APLIKASI": result.data[data].NO_APLIKASI,
+    //         "NAMA_DEBITUR": result.data[data].NAMA_DEBITUR,
+    //         "GENDER": result.data[data].GENDER,
+    //         "ALAMAT_DEBITUR": result.data[data].ALAMAT_DEBITUR,
+    //         "ALAMAT_AGUNAN": result.data[data].ALAMAT_AGUNAN,
+    //         "TANGGAL_LAHIR": result.data[data].TANGGAL_LAHIR,
+    //         "USIA": parseInt(result.data[data].USIA),
+    //         "PK_DATE": result.data[data].PK_DATE,
+    //         "JANGKA_WAKTU": result.data[data].JANGKA_WAKTU,
+    //         "JENIS_KREDIT": result.data[data].JENIS_KREDIT,
+    //         "PLAFOND_KREDIT": parseInt(result.data[data].PLAFOND_KREDIT),
+    //         "HARGA_BANGUNAN": parseInt(result.data[data].HARGA_BANGUNAN),
+    //         "PEKERJAAN": result.data[data].PEKERJAAN,
+    //         "ASURANSI_JIWA": parseInt(result.data[data].ASURANSI_JIWA),
+    //         "ASURANSI_FIRE": parseInt(result.data[data].ASURANSI_FIRE),
+    //         "ASURANSI_KREDIT": parseInt(result.data[data].ASURANSI_KREDIT),
+    //         "DATE_TIME_CREATE": "2018-02-14T11:40:48.637Z"
+    //       }, function (err, inst) {
+  
+    //       });
+    //     };
+    //     for (var data in result.data) {
+    //       Mstcustomer.create({
+    //         "CUSTOMER_ID": result.data[data].NO_APLIKASI,
+    //         "KODE_CABANG": result.data[data].KODE_CABANG,
+    //         "CABANG": result.data[data].CABANG,
+    //         "KANWIL": result.data[data].KANWIL,
+    //         "NO_APLIKASI": result.data[data].NO_APLIKASI,
+    //         "NAMA_DEBITUR": result.data[data].NAMA_DEBITUR,
+    //         "GENDER": result.data[data].GENDER,
+    //         "ALAMAT_DEBITUR": result.data[data].ALAMAT_DEBITUR,
+    //         "ALAMAT_AGUNAN": result.data[data].ALAMAT_AGUNAN,
+    //         "TANGGAL_LAHIR": result.data[data].TANGGAL_LAHIR,
+    //         "USIA": parseInt(result.data[data].USIA),
+    //         "PK_DATE": result.data[data].SP3K_DATE,
+    //         "JANGKA_WAKTU": result.data[data].JANGKA_WAKTU,
+    //         "JENIS_KREDIT": result.data[data].JENIS_KREDIT,
+    //         "PLAFOND_KREDIT": parseInt(result.data[data].PLAFOND_KREDIT),
+    //         "HARGA_BANGUNAN": parseInt(result.data[data].HARGA_BANGUNAN),
+    //         "PEKERJAAN": result.data[data].PEKERJAAN,
+    //         "ASURANSI_JIWA": parseInt(result.data[data].ASURANSI_JIWA),
+    //         "ASURANSI_FIRE": parseInt(result.data[data].ASURANSI_FIRE),
+    //         "ASURANSI_KREDIT": parseInt(result.data[data].ASURANSI_KREDIT),
+    //         "DATE_TIME_CREATE": "2018-02-14T11:40:48.637Z"
+    //       }, function (err, inst) {
+  
+    //       });
+    //     };
+        
+    //    var data={
+    //      message: result.message,
+    //      error:err||null
+    //    }
+    //     cb(err, data);
+    //   });
+    // };
+  
+    // Soapcustomer.remoteMethod(
+    //   'getListBTNConsumerToday', {
+    //     accepts: [{
+    //       arg: 'datatype',
+    //       type: 'string'
+    //     }, {
+    //       arg: 'datetoday',
+    //       type: 'string'
+    //     }],
+    //     returns: {
+    //       arg: 'data',
+    //       type: 'object',
+    //       root: true
+    //     },
+    //     http: {
+    //       verb: 'post',
+    //       path: '/getlistconsumertoday'
+    //     }
+    //   });
+    
+
 };
